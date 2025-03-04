45f20f2ae1054e8d71bf0f5315e8247d2caa420c
jsrozner <1113285+jsrozner@users.noreply.github.com>
2025-03-04 15:05:53 -0800

    add reqs and setup.py

---
+++ readme.md
@@ -0,0 +1,5 @@
+# todo:
+- need to setup requirements.txt; current version may have too many
+
+# usage
+pip install --no-build-isolation git+https://github.com/jsrozner/rozlib-python.git
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rozlib-python",
+    version="0.1.0",
+    description="Joshua Rozner's personal python library.",
+    author="Joshua Rozner",
+    packages=find_packages(),
+)
